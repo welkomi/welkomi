efd1535b47a8e7d34580e4b65d9be0fef5ec7741
Santiago Sánchez <chadsfather@gmail.com>
2015-12-17 20:00:00 +0100

    Cambios en el contenedor de redis

--- wrappers/rediswrapper/index.js
+++ wrappers/rediswrapper/index.js
@@ -2,7 +2,8 @@
  * Created by ssanchez on 17/12/15.
  */
 
-var redis = require('redis').createClient('redis://h:p2koi7dr5v9sv88g4pqm7c8vge@ec2-107-22-174-233.compute-1.amazonaws.com:7599');
+var redis = require('redis')
+    .createClient('redis://h:p2koi7dr5v9sv88g4pqm7c8vge@ec2-107-22-174-233.compute-1.amazonaws.com:7599');
 
 redis.on('error', function (err) {
     console.log('Error:' + err);
