dcc92bcfdae2a04e8f80116c13978a00c2192fde
iraklitbz <iraklitbz@gmail.com>
2016-01-19 17:53:47 +0100

    gallery CSS changes

--- statics/js/tourpublish.js
+++ statics/js/tourpublish.js
@@ -4,12 +4,11 @@
 
 function TourPublishCtrl ($rootScope, $scope) {
   $scope.images = [
-        {thumb: '/statics/images/1.jpg', img: '/statics/images/1.jpg'},
-            {thumb: '/statics/images/2.jpg', img: '/statics/images/2.jpg'},
-            {thumb: '/statics/images/3.jpg', img: '/statics/images/3.jpg'},
-            {thumb: '/statics/images/4.jpg', img: '/statics/images/4.jpg'},
-            {thumb: '/statics/images/5.jpg', img: '/statics/images/5.jpg'},
-            {thumb: '/statics/images/6.jpg', img: '/statics/images/6.jpg'}
+        {thumb: '/statics/images/tourimages/thumbs/1.jpg', img: '/statics/images/tourimages/1.jpg'},
+            {thumb: '/statics/images/tourimages/thumbs/2.jpg', img: '/statics/images/tourimages/2.jpg'},
+            {thumb: '/statics/images/tourimages/thumbs/3.jpg', img: '/statics/images/tourimages/3.jpg'},
+            {thumb: '/statics/images/tourimages/thumbs/4.jpg', img: '/statics/images/tourimages/4.jpg'},
+            {thumb: '/statics/images/tourimages/thumbs/5.jpg', img: '/statics/images/tourimages/5.jpg'},
       ];
 
 }
