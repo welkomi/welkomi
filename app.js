f214e00d0a1d4d0bf4c495558653d47d6995f349
Santiago Sánchez <chadsfather@gmail.com>
2016-03-28 11:31:56 +0200

    Se implemento api de cache y se cacheo el listado de lenguajes disponibles.

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ idioms.getAvailableLangs(function () {
     app.use(function (req, res, next) {
         res.locals.renderGdriveUrl = function (folder, file) {
             return 'http://googledrive.com/host/' + folder + '/' + file;
-        }
+        };
 
         next();
     });
