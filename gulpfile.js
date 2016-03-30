c42b86d82767f8a1dac99436013ed50c21b2211c
welkomi <welkomicompany@gmail.com>
2016-03-30 22:09:02 +0200

    Se implemento minificador en el fichero gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,14 @@
  * Created by ssanchez on 28/03/16.
  */
 var gulp = require('gulp'),
-    sass = require('gulp-sass');
+    sass = require('gulp-sass'),
+    cleancss = require('gulp-clean-css');
 
 gulp.task('css', function () {
     return gulp
         .src('./statics/css/*.scss')
         .pipe(sass().on('error', sass.logError))
+        .pipe(cleancss())
         .pipe(gulp.dest('./statics/css/'));
 });
 
