8f9842fcc8c02b905457989123a4aa0a1373d468
Santiago Sánchez <chadsfather@gmail.com>
2016-04-08 16:19:20 +0200

    Se implemento minificador de archivos js, concatendor.

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@
  */
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
-    cleancss = require('gulp-clean-css');
+    cleancss = require('gulp-clean-css'),
+    uglify = require('gulp-uglifyjs');
 
 gulp.task('css', function () {
     return gulp
@@ -14,6 +15,29 @@ gulp.task('css', function () {
             console.log('Minified size: ' + details.stats.minifiedSize);
         }))
         .pipe(gulp.dest('./statics/css/'));
+});
+
+gulp.task('js', function () {
+    return gulp
+        .src([
+            './statics/js/jquery-1.11.1.min.js',
+            './statics/js/bootstrap.js',
+            './statics/js/liveicons.js',
+            './statics/js/icons.js',
+            './statics/js/angular.min.js',
+            './statics/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.concat.min.js',
+            './statics/ng-scrollbars/dist/scrollbars.min.js',
+            './statics/js/app.js',
+            './statics/js/parallax.js',
+            './statics/js/FBFactory.js',
+            './statics/js/ngGallery.js',
+            './statics/angular-bootstrap/ui-bootstrap.min.js',
+            './statics/angular-bootstrap/ui-bootstrap-tpls.min.js'
+        ])
+        .pipe(uglify('welkomiapp.js', {
+            'mangle': false
+        }))
+        .pipe(gulp.dest('./statics/js/'))
 });
 
 gulp.task('css:watch', function () {
