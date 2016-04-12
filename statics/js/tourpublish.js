243bc851d0d57bc1eb7927a2f9a6829914039c2d
System Administrator <root@Iraklis-MacBook-Pro.local>
2016-04-12 17:38:49 +0200

    CSS

--- statics/js/tourpublish.js
+++ statics/js/tourpublish.js
@@ -8,12 +8,17 @@ function TourPublishCtrl($rootScope, $scope) {
         {thumb: '/statics/images/tourimages/thumbs/2.jpg', img: '/statics/images/tourimages/2.jpg'},
         {thumb: '/statics/images/tourimages/thumbs/3.jpg', img: '/statics/images/tourimages/3.jpg'},
         {thumb: '/statics/images/tourimages/thumbs/4.jpg', img: '/statics/images/tourimages/4.jpg'},
-        {thumb: '/statics/images/tourimages/thumbs/5.jpg', img: '/statics/images/tourimages/5.jpg'},
+        {thumb: '/statics/images/tourimages/thumbs/5.jpg', img: '/statics/images/tourimages/5.jpg'}
     ];
 
     /**
      * Funciones para le datepicker
      */
+    $scope.dateOptions = {
+        showWeeks: false
+    };
+    
+    
     $scope.today = function () {
         $scope.dt = new Date();
     };
