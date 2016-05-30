3019d093c6d3b1f928e6bf8d2550f45b62b354f3
Santiago Sánchez <chadsfather@gmail.com>
2016-05-30 19:17:45 +0200

    javascript innecesario eliminado.

--- statics/js/profile.js
+++ statics/js/profile.js
@@ -2,40 +2,7 @@
  * Created by chadsfather on 9/4/16.
  */
 
-function ProfileCtrl ($rootScope, $scope) {
-    $scope.interface = {};
-    $scope.uploadCount = 0;
-    $scope.success = false;
-    $scope.error = false;
-    $scope.interface.FILE_TYPES = {
-        VALID: 1,
-        INVALID: 2,
-        DELETED: 4,
-        UPLOADED: 8
-    };
-
-    $scope.$on('$dropletReady', function whenDropletReady() {
-        $scope.interface.allowedExtensions([
-            'png',
-            'jpg'
-        ]);
-    });
-
-    $scope.$on('$dropletFileAdded', function onDropletAddedFile (event, file) {
-        console.log(file);
-    });
-
-    $scope.$on('$dropletSuccess', function onDropletSuccess(event, response, files) {
-        $scope.uploadCount = files.length;
-        $scope.success     = true;
-
-        console.log(response, files);
-
-        $timeout(function timeout() {
-            $scope.success = false;
-        }, 5000);
-    });
-}
+function ProfileCtrl ($rootScope, $scope) {}
 
 /**
  * Directiva para manter fixed
@@ -60,9 +27,8 @@ app
         '$rootScope',
         '$scope',
         ProfileCtrl
-    ]).
-
-  directive('scrolltofixed', [
+    ])
+    .directive('scrolltofixed', [
         '$rootScope',
         scrolltofixed
     ]);
