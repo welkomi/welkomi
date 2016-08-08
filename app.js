2e843ef0d9193271e7278db58ead36487e856299
Santiago Sánchez <chadsfather@gmail.com>
2016-08-08 15:27:53 +0200

    fixes

--- app.js
+++ app.js
@@ -158,7 +158,10 @@ idioms.getAvailableLangs(function () {
         
         socket.on('move', function (data) {
             clientsSocket.forEach(function (id) {
-                if (io.sockets.connected[id]) {
+                if (
+                    id !== data.id
+                    && io.sockets.connected[id]
+                ) {
                     io.sockets.connected[id].emit('square', data);
                 }
             });
